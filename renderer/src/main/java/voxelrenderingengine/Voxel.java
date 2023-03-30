ebbb989909935cc25f805c8d87f9fe6f6f26299d
Isabelle <meguminperson@gmail.com>
2023-03-30 20:46:53 +0100

    Renamed from momentum vector to motion vector.

--- renderer/src/main/java/voxelrenderingengine/Voxel.java
+++ renderer/src/main/java/voxelrenderingengine/Voxel.java
@@ -4,8 +4,8 @@ public class Voxel {
     // The voxels position vector.
     public Vector position;
 
-    // The voxels momentum vector (movement in terms of m/s)
-    public Vector momentum;
+    // The voxels motion vector (movement in terms of m/s)
+    public Vector motion;
 
     // The voxels mass in kg.
     public double mass;
@@ -15,13 +15,13 @@ public class Voxel {
 
     /**
      * @param positionVector the voxels starting position vector.
-     * @param momentumVector the voxels starting momentum vector.
+     * @param motionVector the voxels starting motion vector.
      * @param mass the voxels mass.
      * @param size the voxels size.
      */
-    public Voxel(Vector positionVector, Vector momentumVector, double mass, double size) {
+    public Voxel(Vector positionVector, Vector motionVector, double mass, double size) {
         this.position = positionVector;
-        this.momentum = momentumVector;
+        this.motion = motionVector;
         this.mass = mass;
         this.size = size;
     }
