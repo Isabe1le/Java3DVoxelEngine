dbdec248170fd94ce31484282944e1bea3fbc62e
Isabelle <meguminperson@gmail.com>
2023-03-30 20:43:13 +0100

    Made custom Vector class and renamed from motion to momentum.

--- renderer/src/main/java/voxelrenderingengine/App.java
+++ renderer/src/main/java/voxelrenderingengine/App.java
@@ -4,15 +4,9 @@ public class App {
     public static void main(String[] args) {
         Engine engine = new Engine(100, 100, 5);
 
-        double[] voxelPositionVector = new double[3];
-        voxelPositionVector[0] = 10;
-        voxelPositionVector[1] = 15;
-        voxelPositionVector[2] = 30;
+        Vector voxelPositionVector = new Vector(10, 15, 30);
 
-        double[] voxelMovementVector = new double[3];
-        voxelMovementVector[0] = 10;
-        voxelMovementVector[1] = 15;
-        voxelMovementVector[2] = 30;
+        Vector voxelMovementVector = new Vector(10, 15, 30);
 
         Voxel voxel = new Voxel(voxelMovementVector, voxelPositionVector, 10.0, 5.0);
         engine.addVoxel(voxel);
--- renderer/src/main/java/voxelrenderingengine/Voxel.java
+++ renderer/src/main/java/voxelrenderingengine/Voxel.java
@@ -1,57 +1,28 @@
 package voxelrenderingengine;
 
 public class Voxel {
-    // The voxels (x, y, z) coordinates.
-    private double[] positionVector = new double[3];
+    // The voxels position vector.
+    public Vector position;
 
-    // The voxels movement vector (movement in terms of m/s for (x, y, z))
-    private double[] movementVector = new double[3];
+    // The voxels momentum vector (movement in terms of m/s)
+    public Vector momentum;
 
     // The voxels mass in kg.
-    private double mass;
+    public double mass;
 
     // Side length of the voxel.
-    private double size;
+    public double size;
 
     /**
      * @param positionVector the voxels starting position vector.
-     * @param movementVector the voxels starting movement vector.
+     * @param momentumVector the voxels starting momentum vector.
      * @param mass the voxels mass.
      * @param size the voxels size.
      */
-    public Voxel(double[] positionVector, double[] movementVector, double mass, double size) {
-        this.positionVector = positionVector;
-        this.movementVector = movementVector;
+    public Voxel(Vector positionVector, Vector momentumVector, double mass, double size) {
+        this.position = positionVector;
+        this.momentum = momentumVector;
         this.mass = mass;
         this.size = size;
     }
-
-    /**
-     * @return double[3] of the voxels position vector.
-     */
-    public double[] getPositionVector() {
-        return this.positionVector;
-    }
-
-    /**
-     * @return double[3] of the voxels movement vector.
-     */
-    public double[] getMovementVector() {
-        return this.movementVector;
-    }
-
-    /**
-     * @return double of the voxels mass.
-     */
-    public double getMass() {
-        return this.mass;
-    }
-
-    /**
-     * @return double of the voxels size.
-     */
-    public double getSize() {
-        return this.size;
-    }
-
 }
